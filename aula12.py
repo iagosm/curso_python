99b2175a6eb82f27085cd10c9962ac7da5ebc545
Iago Sousa <iagosousa201486@gmail.com>
2023-07-26 08:20:23 -0300

    modulo 1

---
+++ aula12.py
@@ -0,0 +1,6 @@
+nome = "Iago"
+altura = 1.65
+peso = 67
+imc = peso / (altura * altura)
+print(nome,'tem',altura,'de altura')
+print('pesa',peso, 'quilos e seu imc é ', imc)
---
+++ aula13.py
@@ -0,0 +1,10 @@
+nome = "Iago"
+altura = 1.65
+peso = 67
+imc = peso / (altura * altura)
+
+linha_1 = f'{nome} tem {altura:,.2f} de altura'
+linha_2 = f'pesa {peso} quilos e seu imc é {imc:.2f}'
+print(linha_1)
+print(linha_2)
+ 
